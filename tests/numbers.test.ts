81a909a01e76528c9a964f8ce19a662d23805a36
Dobroslav Radosavljevic <dobroslav.radosavljevic@gmail.com>
2025-09-26 01:08:43 +0200

    refactor: update mapRange test cases to use object parameters and remove object utility tests

--- tests/numbers.test.ts
+++ tests/numbers.test.ts
@@ -96,9 +96,15 @@ describe("Number Utilities", () => {
   });
 
   test("mapRange", () => {
-    expect(mapRange(5, 0, 10, 0, 100)).toBe(50);
-    expect(mapRange(0, 0, 10, 0, 100)).toBe(0);
-    expect(mapRange(10, 0, 10, 0, 100)).toBe(100);
+    expect(mapRange(5, { fromMin: 0, fromMax: 10, toMin: 0, toMax: 100 })).toBe(
+      50
+    );
+    expect(mapRange(0, { fromMin: 0, fromMax: 10, toMin: 0, toMax: 100 })).toBe(
+      0
+    );
+    expect(
+      mapRange(10, { fromMin: 0, fromMax: 10, toMin: 0, toMax: 100 })
+    ).toBe(100);
   });
 
   test("formatBytes", () => {
